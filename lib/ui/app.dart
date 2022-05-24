e4b74149ecc36f8bec4632be92ccf9f15be31a1d
Flavian Magrit <pro.flavianmagrit@gmail.com>
2022-05-24 13:00:29 +0200

    feat: update files

---
+++ lib/ui/app.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mautus_flutter/ui/screens/login/login_page.dart';
+
+import '../main.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}

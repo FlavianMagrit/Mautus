d76d040a45b84f6a9b15f51423bc42bc8ca3347c
Flavian Magrit <pro.flavianmagrit@gmail.com>
2022-05-27 18:04:35 +0200

    feat: change color

--- lib/ui/widgets/custom_row.dart
+++ lib/ui/widgets/custom_row.dart
@@ -4,6 +4,7 @@ import 'package:hexcolor/hexcolor.dart';
 
 class CustomRow extends StatefulWidget {
   final String mystring;
+  String color = '#0FF000';
 
   CustomRow({Key? key, required this.mystring}) : super(key: key);
 
@@ -16,7 +17,6 @@ class _CustomRowState extends State<CustomRow> {
 
   @override
   Widget build(BuildContext context) {
-
     print("response: $response");
 
     if (widget.mystring == response) {
@@ -27,7 +27,7 @@ class _CustomRowState extends State<CustomRow> {
 
     List<Widget> lettersList = [];
 
-    void _incrementString(String letter) {
+    void _incrementString(String letter, int i) {
       setState(() {
         if (response!.isEmpty) {
           if (widget.mystring != 'null') {
@@ -35,18 +35,25 @@ class _CustomRowState extends State<CustomRow> {
           }
         }
         response = response! + letter;
+
         print('reponseSetState: $response');
-      });
+      }
+      );
+      if (response![i] == widget.mystring[i]) {
+        widget.color = '#FF0000';
+      }
     }
 
+    print('reponse: $response');
     for (int i = 1; i < widget.mystring.length; i++) {
+      print('widget.mystring[i]: ${widget.mystring[i]}');
       lettersList.add(
         Container(
             height: 50,
             width: 50,
             margin: const EdgeInsets.only(left: 2.0, bottom: 3.0),
             alignment: Alignment.center,
-            color: HexColor("#013a79"),
+            color: HexColor(widget.color),
             child: TextField(
               inputFormatters: [
                 UpperCaseTextFormatter(),
@@ -60,7 +67,7 @@ class _CustomRowState extends State<CustomRow> {
               ),
               onChanged: (value) {
                 value.length == 1 && FocusScope.of(context).nextFocus();
-                _incrementString(value);
+                _incrementString(value, i);
               },
               decoration: const InputDecoration(
                 border: InputBorder.none,
@@ -71,33 +78,33 @@ class _CustomRowState extends State<CustomRow> {
     }
 
     return Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-                height: 50,
-                width: 50,
-                margin: const EdgeInsets.only(left: 2.0, bottom: 3.0),
-                alignment: Alignment.center,
-                color: HexColor("#013a79"),
-                child: Text(
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    '${widget.mystring[0]}'.toUpperCase())),
-            Row(children: lettersList),
-          ],
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+              height: 50,
+              width: 50,
+              margin: const EdgeInsets.only(left: 2.0, bottom: 3.0),
+              alignment: Alignment.center,
+              color: HexColor("#013a79"),
+              child: Text(
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  '${widget.mystring[0]}'.toUpperCase())),
+          Row(children: lettersList),
+        ],
+      ),
     );
   }
 }
 
 class UpperCaseTextFormatter extends TextInputFormatter {
   @override
-  TextEditingValue formatEditUpdate(
-      TextEditingValue oldValue, TextEditingValue newValue) {
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue,
+      TextEditingValue newValue) {
     return TextEditingValue(
       text: newValue.text.toUpperCase(),
       selection: newValue.selection,

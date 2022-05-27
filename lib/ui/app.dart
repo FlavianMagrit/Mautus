92317bf8cc1987e00f561c3eab89c6bb427782c5
Flavian Magrit <pro.flavianmagrit@gmail.com>
2022-05-27 12:30:30 +0200

    feat: custom homepage

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,19 +1,21 @@
 import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:mautus_flutter/ui/screens/signin_and_signup/sign_in_and_sign_up_page.dart';
 
 import 'screens/home/home_page.dart';
 
 class App extends StatelessWidget {
   App({Key? key}) : super(key: key);
 
-  final BeamerDelegate routerDelegate = BeamerDelegate(locationBuilder: RoutesLocationBuilder(
-    routes: {
-      // '/': (context, state, data) => const SignInSignUpPage(title: 'Connexion'),
-      '/': (context, state, data) => const HomePage(title: 'Maumau Mautus',),
-      '/home': (context, state, data) => const HomePage(title: 'Maumau Mautus',)
-    }
-  ));
+  final BeamerDelegate routerDelegate = BeamerDelegate(
+      locationBuilder: RoutesLocationBuilder(routes: {
+    '/': (context, state, data) => const SignInSignUpPage(title: 'Connexion'),
+    // '/': (context, state, data) => const HomePage(title: 'Maumau Mautus',),
+    '/home': (context, state, data) => const HomePage(
+          title: 'Maumau Mautus',
+        )
+  }));
 
   @override
   Widget build(BuildContext context) {
--- lib/ui/widgets/custom_input.dart
+++ lib/ui/widgets/custom_input.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class CustomInput extends StatelessWidget {
-  CustomInput({Key? key, required this.inputLabel}) : super(key: key);
+  CustomInput({
+    Key? key,
+    required this.inputLabel,
+    required this.controller,
+    this.keyboardType,
+    this.obscureText = true,
+    this.enableSuggestions = false,
+    this.autocorrect = false,
+  }) : super(key: key);
+
   String inputLabel;
+  var controller;
+  var keyboardType;
+  bool obscureText;
+  bool enableSuggestions;
+  bool autocorrect;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(10),
       child: TextField(
-        // controller: nameController,
-        decoration: InputDecoration(
-          border: const OutlineInputBorder(),
-          labelText: inputLabel,
-        ),
+        keyboardType: keyboardType,
+        controller: controller,
+        obscureText: obscureText,
+        enableSuggestions: enableSuggestions,
+        autocorrect: autocorrect,
+        decoration: const InputDecoration(
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white, width: 2.0),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white, width: 2.0),
+            )),
+        style: const TextStyle(color: Colors.white),
       ),
     );
   }
